45d67fe9bd4c7bcba6d38722127f188878f545cc
Faheema <faheema@loyalitsolutions.com>
2025-02-28 17:59:08 +0530

    [ADD]: JOURNAL ALLOCATION MODULE ADDED

--- misc_journal_allocation/wizard/payment_allocation_wizard.py
+++ misc_journal_allocation/wizard/payment_allocation_wizard.py
@@ -20,8 +20,7 @@ class JournalAllocation(models.TransientModel):
     invoice_allocation_ids = fields.One2many('journal.allocation.wizard.debit.lines', 'rec_id', "Invoices")
     journal_allocation_ids = fields.One2many('journal.allocation.wizard.credit.lines', 'rec_id', "Journals")
     move_line_id = fields.Many2one('account.move.line', related='invoice_allocation_ids.move_line_id.move_id')
-    payment_type = fields.Selection([('inbound', 'Receive Money'), ('outbound', 'Send Money')], string="Payment Type",
-                                    required=False)
+
     show_reference = fields.Boolean(copy=False)
 
     # @api.onchange('show_parent_child')
@@ -352,117 +351,129 @@ class JournalAllocation(models.TransientModel):
     #                         pay_vals.append((0, 0, vals))
     #                     data.payment_allocation_ids = pay_vals
     #
-    # def get_matching_dict(self, debit_move_dict, credit_move_dict):
-    #     matching_list = []
-    #     debit_dict = {}
-    #     credit_dict = {}
-    #     payment_type = self.payment_type
-    #     if payment_type == 'inbound':
-    #         debit_dict = debit_move_dict
-    #         credit_dict = credit_move_dict
-    #     elif payment_type == 'outbound':
-    #         debit_dict = credit_move_dict
-    #         credit_dict = debit_move_dict
-    #     if payment_type in ('inbound', 'outbound'):
-    #         for cred_move_id, credamt, in credit_dict.items():
-    #             if credamt > 0.0:
-    #                 for deb_move_id, debamt in debit_dict.items():
-    #                     is_full_reconcile = debit_dict['is_full_reconcile']
-    #                     balance = credamt - debamt
-    #                     if credamt <= 0.0:
-    #                         continue
-    #                     if debamt == 0.0:
-    #                         continue
-    #                     if balance > 0.0:
-    #                         ful_rec = self.env['account.full.reconcile']
-    #                         if is_full_reconcile == True:
-    #                             vals = {}
-    #                             ful_rec = self.env['account.full.reconcile'].create(vals)
-    #                         if deb_move_id != 'is_full_reconcile':
-    #                             matching_list.append(
-    #                                 {'credit_move_id': cred_move_id, 'full_reconcile_id': ful_rec.id,
-    #                                  'debit_amount_currency': debamt, 'credit_amount_currency': debamt,
-    #                                  'debit_move_id': deb_move_id, 'amount': debamt})
-    #                         debit_dict[deb_move_id] = 0.0
-    #
-    #                     if balance < 0.0:
-    #                         ful_rec = self.env['account.full.reconcile']
-    #                         if is_full_reconcile == True:
-    #                             vals = {}
-    #                             ful_rec = self.env['account.full.reconcile'].create(vals)
-    #                         if deb_move_id != 'is_full_reconcile':
-    #                             matching_list.append(
-    #                                 {'credit_move_id': cred_move_id, 'full_reconcile_id': ful_rec.id,
-    #                                  'debit_amount_currency': credamt, 'credit_amount_currency': credamt,
-    #                                  'debit_move_id': deb_move_id, 'amount': credamt})
-    #                         debit_dict[deb_move_id] = abs(balance)
-    #                         credit_dict[cred_move_id] = 0.0
-    #
-    #
-    #                     if balance == 0.0:
-    #                         ful_rec = self.env['account.full.reconcile']
-    #                         if is_full_reconcile == True:
-    #                             vals = {}
-    #                             ful_rec = self.env['account.full.reconcile'].create(vals)
-    #
-    #                         if deb_move_id != 'is_full_reconcile':
-    #                             matching_list.append(
-    #                                 {'credit_move_id': cred_move_id, 'full_reconcile_id': ful_rec.id,
-    #                                  'debit_move_id': deb_move_id, 'debit_amount_currency': debamt,
-    #                                  'credit_amount_currency': debamt, 'amount': debamt})
-    #                         debit_dict[deb_move_id] = 0.0
-    #                         credit_dict[cred_move_id] = 0.0
-    #                         credamt = 0.0
-    #                     credamt = balance
-    #     return matching_list
-    #
+    def get_matching_dict(self,payment_type, debit_move_dict, credit_move_dict):
+        matching_list = []
+        debit_dict = {}
+        credit_dict = {}
+        # payment_type = self.payment_type
+        if payment_type == 'inbound':
+            debit_dict = debit_move_dict
+            credit_dict = credit_move_dict
+        elif payment_type == 'outbound':
+            debit_dict = credit_move_dict
+            credit_dict = debit_move_dict
+        if payment_type in ('inbound', 'outbound'):
+            for cred_move_id, credamt, in credit_dict.items():
+                if credamt > 0.0:
+                    for deb_move_id, debamt in debit_dict.items():
+                        is_full_reconcile = debit_dict['is_full_reconcile']
+                        balance = credamt - debamt
+                        if credamt <= 0.0:
+                            continue
+                        if debamt == 0.0:
+                            continue
+                        if balance > 0.0:
+                            ful_rec = self.env['account.full.reconcile']
+                            if is_full_reconcile == True:
+                                vals = {}
+                                ful_rec = self.env['account.full.reconcile'].create(vals)
+                            if deb_move_id != 'is_full_reconcile':
+                                matching_list.append(
+                                    {'credit_move_id': cred_move_id, 'full_reconcile_id': ful_rec.id,
+                                     'debit_amount_currency': debamt, 'credit_amount_currency': debamt,
+                                     'debit_move_id': deb_move_id, 'amount': debamt})
+                            debit_dict[deb_move_id] = 0.0
+
+                        if balance < 0.0:
+                            ful_rec = self.env['account.full.reconcile']
+                            if is_full_reconcile == True:
+                                vals = {}
+                                ful_rec = self.env['account.full.reconcile'].create(vals)
+                            if deb_move_id != 'is_full_reconcile':
+                                matching_list.append(
+                                    {'credit_move_id': cred_move_id, 'full_reconcile_id': ful_rec.id,
+                                     'debit_amount_currency': credamt, 'credit_amount_currency': credamt,
+                                     'debit_move_id': deb_move_id, 'amount': credamt})
+                            debit_dict[deb_move_id] = abs(balance)
+                            credit_dict[cred_move_id] = 0.0
+
+
+                        if balance == 0.0:
+                            ful_rec = self.env['account.full.reconcile']
+                            if is_full_reconcile == True:
+                                vals = {}
+                                ful_rec = self.env['account.full.reconcile'].create(vals)
+
+                            if deb_move_id != 'is_full_reconcile':
+                                matching_list.append(
+                                    {'credit_move_id': cred_move_id, 'full_reconcile_id': ful_rec.id,
+                                     'debit_move_id': deb_move_id, 'debit_amount_currency': debamt,
+                                     'credit_amount_currency': debamt, 'amount': debamt})
+                            debit_dict[deb_move_id] = 0.0
+                            credit_dict[cred_move_id] = 0.0
+                            credamt = 0.0
+                        credamt = balance
+        return matching_list
+
     def validate_payment(self):
-        print()
-    #     payment_amount = 0
-    #     for rec in self.payment_allocation_ids:
-    #         payment_amount += rec.amount
-    #     for rec in self.invoice_allocation_ids:
-    #         if rec.inv_allocate_amount >rec.inv_unallocated_amount:
-    #             raise ValidationError(_('Allocate amount exceeds invoice amount'))
-    #         if rec.inv_allocate_amount > payment_amount:
-    #             raise ValidationError(_('Allocate amount exceeds payment amount'))
-    #
-    #     debit_move_dict = {}
-    #     credit_move_dict = {}
-    #     for line in self.invoice_allocation_ids:
-    #         # debit_move_dict[line.move_line_id.id] = line.inv_allocate_amount
-    #         val_2 = line.move_line_id.id
-    #         if line.move_line_id.move_id.move_type == 'out_invoice':
-    #             if line.move_line_id.move_id.line_ids.filtered(lambda l: l.credit == 0):
-    #                 val_2 = line.move_line_id.move_id.line_ids.filtered(lambda l: l.credit == 0)[0].id
-    #         elif line.move_line_id.move_id.move_type == 'in_invoice':
-    #             if line.move_line_id.move_id.line_ids.filtered(lambda l: l.debit == 0):
-    #                 val_2 = line.move_line_id.move_id.line_ids.filtered(lambda l: l.debit == 0)[0].id
-    #         debit_move_dict[val_2] = line.inv_allocate_amount
-    #
-    #     for line in self.payment_allocation_ids:
-    #         credit_move_dict[line.move_line_id.id] = self.balnc_paymnt_amnt
-    #
-    #     for line in self.invoice_allocation_ids:
-    #         if self.payment_type == "inbound":
-    #             if line.inv_unallocated_amount == line.inv_allocate_amount:
-    #                 debit_move_dict['is_full_reconcile'] = True
-    #             else:
-    #                 debit_move_dict['is_full_reconcile'] = False
-    #         else:
-    #             if line.inv_unallocated_amount == line.inv_allocate_amount:
-    #                 credit_move_dict['is_full_reconcile'] = True
-    #             else:
-    #                 credit_move_dict['is_full_reconcile'] = False
-    #     matching_list = self.get_matching_dict(debit_move_dict, credit_move_dict)
-    #     for rec_val in matching_list:
-    #         rec = self.env['account.partial.reconcile'].create(rec_val)
+        print(":validate" ,self.balnc_paymnt_amnt)
+        payment_amount = 0
+        payment_type=False
+        for rec in self.journal_allocation_ids:
+            payment_amount += rec.amount
+            if self.balnc_paymnt_amnt ==0:
+                raise ValidationError(_('Already Allocated'))
+
+
+        for rec in self.invoice_allocation_ids:
+            if rec.inv_allocate_amount >rec.inv_unallocated_amount:
+                raise ValidationError(_('Allocate amount exceeds invoice amount'))
+            if rec.inv_allocate_amount > payment_amount:
+                raise ValidationError(_('Allocate amount exceeds journal amount'))
+
+        debit_move_dict = {}
+        credit_move_dict = {}
+        for line in self.invoice_allocation_ids:
+            print("--invoive allocation")
+            # debit_move_dict[line.move_line_id.id] = line.inv_allocate_amount
+            val_2 = line.move_line_id.id
+            if line.move_line_id.move_id.move_type == 'out_invoice':
+                if line.move_line_id.move_id.line_ids.filtered(lambda l: l.credit == 0):
+                    val_2 = line.move_line_id.move_id.line_ids.filtered(lambda l: l.credit == 0)[0].id
+            elif line.move_line_id.move_id.move_type == 'in_invoice':
+                if line.move_line_id.move_id.line_ids.filtered(lambda l: l.debit == 0):
+                    val_2 = line.move_line_id.move_id.line_ids.filtered(lambda l: l.debit == 0)[0].id
+            debit_move_dict[val_2] = line.inv_allocate_amount
+
+        for line in self.journal_allocation_ids:
+            print("jounal")
+            payment_type='inbound' if line.move_line_id.account_id.account_type=='asset_receivable' else 'outbound' if line.move_line_id.account_id.account_type=='liability_payable' else ''
+            credit_move_dict[line.move_line_id.id] = self.balnc_paymnt_amnt
+        print("000000")
+        for line in self.invoice_allocation_ids:
+            if payment_type == "inbound":
+                if line.inv_unallocated_amount == line.inv_allocate_amount:
+                    debit_move_dict['is_full_reconcile'] = True
+                else:
+                    debit_move_dict['is_full_reconcile'] = False
+            else:
+                if line.inv_unallocated_amount == line.inv_allocate_amount:
+                    credit_move_dict['is_full_reconcile'] = True
+                else:
+                    credit_move_dict['is_full_reconcile'] = False
+
+        print("payment type",payment_type)
+        matching_list = self.get_matching_dict(payment_type,debit_move_dict, credit_move_dict)
+        print("--matching list",matching_list)
+        for rec_val in matching_list:
+            rec = self.env['account.partial.reconcile'].create(rec_val)
+            print("----rec",rec)
 
 class JournalDebitLines(models.TransientModel):
     _name = "journal.allocation.wizard.debit.lines"
     _description = 'Debit Lines'
 
-    rec_id = fields.Many2one('payment.allocation.wizard')
+    rec_id = fields.Many2one('journal.allocation.wizard')
     invoice_id = fields.Many2one('account.move')
     partner_id = fields.Many2one('res.partner', string="Partner")
     name = fields.Char(string="Invoices")
@@ -471,7 +482,7 @@ class JournalDebitLines(models.TransientModel):
     date_due = fields.Date("Due Date")
     inv_amount = fields.Float('Invoice Amount')
     inv_unallocated_amount = fields.Float('Unallocated Amount')
-    inv_allocate_amount = fields.Float('Allocate Amount')
+    inv_allocate_amount = fields.Float('Allocate Amount',)
     is_invoice_allocate = fields.Boolean(string="Allocate")
 
     balance_amount = fields.Float('Balance')
@@ -480,9 +491,10 @@ class JournalDebitLines(models.TransientModel):
 
     @api.onchange('is_invoice_allocate')
     def onchange_is_invoice_allocate(self):
+        print("----------is invoice allocate")
 
         total_payment = []
-        for vals in self.rec_id.payment_allocation_ids:
+        for vals in self.rec_id.journal_allocation_ids:
             total_payment.append(vals.amount)
 
 
@@ -490,11 +502,15 @@ class JournalDebitLines(models.TransientModel):
 
         if self.is_invoice_allocate:
             for rec in self:
-                if rec.inv_unallocated_amount >= payment_amount:
-                    rec.inv_allocate_amount = payment_amount
-                    payment_amount += rec.inv_unallocated_amount - payment_amount
+                if rec.inv_unallocated_amount >= self.rec_id.balnc_paymnt_amnt:
+                    rec.inv_allocate_amount = self.rec_id.balnc_paymnt_amnt
+
+
+
                 else:
+                    print("elseee",rec.inv_unallocated_amount)
                     rec.inv_allocate_amount = rec.inv_unallocated_amount
+                    print("inv_allocate_amount",rec.inv_allocate_amount)
 
         else:
             self.inv_allocate_amount = 0.0
@@ -504,7 +520,7 @@ class JournalCreditLines(models.TransientModel):
     _name = "journal.allocation.wizard.credit.lines"
     _description = 'Credit Lines'
 
-    rec_id = fields.Many2one('payment.allocation.wizard')
+    rec_id = fields.Many2one('journal.allocation.wizard')
     name = fields.Char(string="Journal")
     move_line_id = fields.Many2one('account.move.line', string="Move Line")
     date = fields.Date(" Date")
